5e74d3b1c926563aa7f8da2f31c8e816c1fcb1ea
123Weber123 <3136988453@qq.com>
2023-12-18 21:09:13 +0800

    Revert "Revert "m61工程""11
    This reverts commit 5cab89d118a0d845b999dbd7b16c436b0f935e67.

---
+++ helloworld/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/bsp/board/bl616dk/board.c" "CMakeFiles/app.dir/D_/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/bsp/board/bl616dk/board.c.obj" "gcc" "CMakeFiles/app.dir/D_/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/bsp/board/bl616dk/board.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/bsp/board/bl616dk/fw_header.c" "CMakeFiles/app.dir/D_/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/bsp/board/bl616dk/fw_header.c.obj" "gcc" "CMakeFiles/app.dir/D_/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/bsp/board/bl616dk/fw_header.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/mui.c" "CMakeFiles/app.dir/u8g2/mui.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/mui.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/mui_u8g2.c" "CMakeFiles/app.dir/u8g2/mui_u8g2.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/mui_u8g2.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_arc.c" "CMakeFiles/app.dir/u8g2/u8g2_arc.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_arc.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_bitmap.c" "CMakeFiles/app.dir/u8g2/u8g2_bitmap.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_bitmap.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_box.c" "CMakeFiles/app.dir/u8g2/u8g2_box.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_box.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_buffer.c" "CMakeFiles/app.dir/u8g2/u8g2_buffer.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_buffer.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_button.c" "CMakeFiles/app.dir/u8g2/u8g2_button.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_button.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_circle.c" "CMakeFiles/app.dir/u8g2/u8g2_circle.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_circle.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_cleardisplay.c" "CMakeFiles/app.dir/u8g2/u8g2_cleardisplay.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_cleardisplay.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_d_memory.c" "CMakeFiles/app.dir/u8g2/u8g2_d_memory.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_d_memory.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_d_setup.c" "CMakeFiles/app.dir/u8g2/u8g2_d_setup.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_d_setup.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_font.c" "CMakeFiles/app.dir/u8g2/u8g2_font.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_font.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_fonts.c" "CMakeFiles/app.dir/u8g2/u8g2_fonts.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_fonts.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_hvline.c" "CMakeFiles/app.dir/u8g2/u8g2_hvline.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_hvline.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_input_value.c" "CMakeFiles/app.dir/u8g2/u8g2_input_value.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_input_value.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_intersection.c" "CMakeFiles/app.dir/u8g2/u8g2_intersection.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_intersection.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_kerning.c" "CMakeFiles/app.dir/u8g2/u8g2_kerning.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_kerning.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_line.c" "CMakeFiles/app.dir/u8g2/u8g2_line.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_line.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_ll_hvline.c" "CMakeFiles/app.dir/u8g2/u8g2_ll_hvline.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_ll_hvline.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_message.c" "CMakeFiles/app.dir/u8g2/u8g2_message.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_message.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_polygon.c" "CMakeFiles/app.dir/u8g2/u8g2_polygon.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_polygon.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_selection_list.c" "CMakeFiles/app.dir/u8g2/u8g2_selection_list.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_selection_list.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8g2_setup.c" "CMakeFiles/app.dir/u8g2/u8g2_setup.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8g2_setup.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8log.c" "CMakeFiles/app.dir/u8g2/u8log.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8log.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8log_u8g2.c" "CMakeFiles/app.dir/u8g2/u8log_u8g2.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8log_u8g2.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8log_u8x8.c" "CMakeFiles/app.dir/u8g2/u8log_u8x8.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8log_u8x8.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_8x8.c" "CMakeFiles/app.dir/u8g2/u8x8_8x8.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_8x8.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_byte.c" "CMakeFiles/app.dir/u8g2/u8x8_byte.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_byte.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_cad.c" "CMakeFiles/app.dir/u8g2/u8x8_cad.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_cad.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_capture.c" "CMakeFiles/app.dir/u8g2/u8x8_capture.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_capture.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_d_ssd1306_128x64_noname.c" "CMakeFiles/app.dir/u8g2/u8x8_d_ssd1306_128x64_noname.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_d_ssd1306_128x64_noname.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_debounce.c" "CMakeFiles/app.dir/u8g2/u8x8_debounce.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_debounce.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_display.c" "CMakeFiles/app.dir/u8g2/u8x8_display.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_display.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_fonts.c" "CMakeFiles/app.dir/u8g2/u8x8_fonts.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_fonts.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_gpio.c" "CMakeFiles/app.dir/u8g2/u8x8_gpio.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_gpio.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_input_value.c" "CMakeFiles/app.dir/u8g2/u8x8_input_value.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_input_value.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_message.c" "CMakeFiles/app.dir/u8g2/u8x8_message.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_message.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_selection_list.c" "CMakeFiles/app.dir/u8g2/u8x8_selection_list.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_selection_list.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_setup.c" "CMakeFiles/app.dir/u8g2/u8x8_setup.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_setup.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_string.c" "CMakeFiles/app.dir/u8g2/u8x8_string.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_string.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_u16toa.c" "CMakeFiles/app.dir/u8g2/u8x8_u16toa.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_u16toa.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2/u8x8_u8toa.c" "CMakeFiles/app.dir/u8g2/u8x8_u8toa.c.obj" "gcc" "CMakeFiles/app.dir/u8g2/u8x8_u8toa.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ helloworld/build/build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/DependInfo.cmake
@@ -0,0 +1,80 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/startup/riscv_fpu.S" "D:/anxinke/AiPi-Open-Kits/helloworld/build/build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/riscv_fpu.S.obj"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/startup/start.S" "D:/anxinke/AiPi-Open-Kits/helloworld/build/build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/start.S.obj"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/startup/vector.S" "D:/anxinke/AiPi-Open-Kits/helloworld/build/build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/vector.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARCH_RISCV"
+  "BFLB_TIMESTAMP_TIMEZONE=8"
+  "BFLB_USE_HAL_DRIVER"
+  "BFLB_USE_ROM_DRIVER"
+  "BL616"
+  "CONFIG_CLI_CMD_ENABLE"
+  "CONFIG_IRQ_NUM=80"
+  "CONFIG_LIBC_FLOAT=1"
+  "CONFIG_LIBC_FLOAT_EX=1"
+  "CONFIG_LIBC_LONG_LONG=1"
+  "CONFIG_LOG_LEVEL=3"
+  "CONFIG_TLSF"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/bsp/board/bl616dk/."
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/libc/newlib/."
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/libc/."
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/mm/."
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/mm/tlsf/."
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/utils/log"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/utils/ring_buffer"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/utils/bflb_block_pool"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/utils/bflb_timestamp"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/components/utils/getopt"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/include"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/include/arch"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/include/arch/risc-v/t-head"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/include/arch/risc-v/t-head/Core/Include"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/include/arch/risc-v/t-head/csi_dsp/include"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/config/bl616"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/flash"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/include"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/include/hardware"
+  "D:/anxinke/AiPi-Open-Kits/helloworld/."
+  "D:/anxinke/AiPi-Open-Kits/helloworld/u8g2"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/port/bl616_clock.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/port/bl616_clock.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/port/bl616_clock.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_aon.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_aon.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_aon.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_common.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_common.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_common.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_ef_cfg.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_ef_cfg.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_ef_cfg.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_pm.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_pm.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_pm.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_psram.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_psram.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_psram.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_romapi_e907.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_romapi_e907.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_romapi_e907.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_romapi_patch.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_romapi_patch.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_romapi_patch.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_sdh.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_sdh.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_sdh.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/src/bl616_tzc_sec.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_tzc_sec.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/src/bl616_tzc_sec.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/startup/interrupt.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/interrupt.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/interrupt.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/startup/start_load.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/start_load.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/start_load.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/soc/bl616/std/startup/system_bl616.c" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/system_bl616.c.obj" "gcc" "build_out/drivers/soc/bl616/std/CMakeFiles/std.dir/startup/system_bl616.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
