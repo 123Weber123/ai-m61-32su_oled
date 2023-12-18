72a61f4484384c956cb189a4c25b5ca59ef30bc4
123Weber123 <3136988453@qq.com>
2023-12-18 20:16:50 +0800

    m61工程
    提交

---
+++ helloworld/build/CMakeFiles/helloworld_bl616.elf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for helloworld_bl616.elf.
+# This may be replaced when dependencies are built.
---
+++ helloworld/build/build_out/drivers/lhal/CMakeFiles/lhal.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/config/bl616/device_table.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/config/bl616/device_table.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/config/bl616/device_table.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/include/arch/risc-v/t-head/rv_hart.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/include/arch/risc-v/t-head/rv_hart.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/include/arch/risc-v/t-head/rv_hart.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/include/arch/risc-v/t-head/rv_pmp.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/include/arch/risc-v/t-head/rv_pmp.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/include/arch/risc-v/t-head/rv_pmp.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_acomp.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_acomp.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_acomp.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_adc.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_adc.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_adc.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_auadc.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_auadc.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_auadc.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_audac.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_audac.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_audac.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_cam.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_cam.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_cam.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_cks.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_cks.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_cks.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_common.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_common.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_common.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_dac.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_dac.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_dac.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_dbi.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_dbi.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_dbi.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_dma.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_dma.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_dma.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_ef_ctrl.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_ef_ctrl.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_ef_ctrl.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_emac.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_emac.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_emac.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_flash.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_flash.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_flash.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_gpio.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_gpio.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_gpio.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_i2c.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_i2c.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_i2c.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_i2s.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_i2s.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_i2s.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_ir.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_ir.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_ir.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_irq.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_irq.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_irq.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_l1c.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_l1c.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_l1c.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_mjpeg.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_mjpeg.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_mjpeg.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_mtimer.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_mtimer.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_mtimer.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_platform_dma.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_platform_dma.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_platform_dma.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_pwm_v2.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_pwm_v2.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_pwm_v2.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_rtc.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_rtc.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_rtc.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_sdio2.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sdio2.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sdio2.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_sec_aes.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sec_aes.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sec_aes.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_sec_sha.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sec_sha.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sec_sha.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_sec_trng.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sec_trng.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_sec_trng.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_spi.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_spi.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_spi.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_timer.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_timer.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_timer.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_uart.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_uart.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_uart.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/bflb_wdg.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_wdg.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/bflb_wdg.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/flash/bflb_sf_cfg.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_sf_cfg.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_sf_cfg.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/flash/bflb_sf_ctrl.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_sf_ctrl.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_sf_ctrl.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/flash/bflb_sflash.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_sflash.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_sflash.c.obj.d"
+  "D:/anxinke/AiPi-Open-Kits/aithinker_Ai-M6X_SDK/drivers/lhal/src/flash/bflb_xip_sflash.c" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_xip_sflash.c.obj" "gcc" "build_out/drivers/lhal/CMakeFiles/lhal.dir/src/flash/bflb_xip_sflash.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
